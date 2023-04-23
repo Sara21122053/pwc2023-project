3f9ab15d19cb7d6f6baed5e1754afcfba59f2b42
Sara21122053 <131326188+Sara21122053@users.noreply.github.com>
2023-04-22 23:11:23 -0500

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Hello!");
+        
+        Console.WriteLine("What is the percentage of your qualification?");
+        string qualification = Console.ReadLine();
+        int value = int.Parse(qualification);
+
+        string letter = "";
+
+        if (value >= 90)
+        {
+            letter = "A";
+        }
+        else if (value >= 80)
+        {
+            letter = "B";
+        }
+        else if (value >= 70)
+        {
+            letter = "C";
+        }
+        else if (value >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your qualification is: {letter}");
+
+        if (value >= 70)
+        {
+            Console.WriteLine("Congratulations!. You passed. Keep up the good work");
+        }
+        else
+        {
+            Console.WriteLine("Don't worry, you'll do better next time.");
+        }
     }
 }
