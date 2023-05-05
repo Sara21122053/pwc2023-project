76967100ded580da8a156fa6865e2545d0d96950
Sara21122053 <131326188+Sara21122053@users.noreply.github.com>
2023-05-05 18:15:45 -0500

    Update Program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.WriteLine("Resume Lucia de la Cruz");
+
+        Job job1 = new Job();
+        job1._jobTitule = "Ballerina";
+        job1._companyName = "English National Ballet School";
+        job1._startYear = 2014;
+        job1._endYear = 2018;
+        //job1.Display();
+
+        Job job2 = new Job();
+        job2._jobTitule = "Ballerina / Assistant choreographer";
+        job2._companyName = "Sadler's Wells Theatre";
+        job2._startYear = 2018;
+        job2._endYear = 2023;
+        //job2.Display();
+
+        Resume luciaResume = new Resume();
+        luciaResume._name = "Lucia de la Cruz";
+        luciaResume._jobs.Add(job1);
+        luciaResume._jobs.Add(job2);
+        luciaResume.Display();
     }
 }
