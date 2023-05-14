48f2fe25856d9e9b32111b19feaf817e0a6ad889
Sara21122053 <131326188+Sara21122053@users.noreply.github.com>
2023-05-14 00:37:53 -0500

    Add files via upload

---
+++ prove/Develop02/Answers.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Answers
+{
+    public string questions;
+    public string answers;
+    public DateTime dateText;
+    public List<string> multimediaFiles = new List<string>();
+}
