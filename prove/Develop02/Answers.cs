7080b569673cd66732399ab40b2f5f2aec5ba5b4
Sara21122053 <131326188+Sara21122053@users.noreply.github.com>
2023-05-24 20:07:23 -0500

    Update Answers.cs

--- prove/Develop02/Answers.cs
+++ prove/Develop02/Answers.cs
@@ -1,9 +1,1 @@
-using System;
 
-public class Answers
-{
-    public string questions;
-    public string answers;
-    public DateTime dateText;
-    public List<string> multimediaFiles = new List<string>();
-}
