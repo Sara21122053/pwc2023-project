f8dfb46433e66b67e27e3e353964ab9a7247cd27
Sara21122053 <131326188+Sara21122053@users.noreply.github.com>
2023-06-01 23:50:31 -0500

    Update Program.cs

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+       Assignment work1 = new Assignment("Harika Zhang", "Multiplication");
+       Console.WriteLine(work1.GetSummary());
+
+       MathAssignment work2 = new MathAssignment("Alena Hamdi", "Fractions", "5.6", "2 - 10");
+       Console.WriteLine(work2.GetSummary());
+       Console.WriteLine(work2.GetHomeworkList());
+
+       WritingAssignment work3 = new WritingAssignment("Olivia Hamdi", "European History", "Beginnings of the Cid and Almoravid expansion");
+       Console.WriteLine(work3.GetSummary());
+       Console.WriteLine(work3.GetWritingInformation());
     }
 }
