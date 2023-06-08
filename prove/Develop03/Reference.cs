0e33f824dbd07ace6cc37d8120e871f7fdeb658c
Sara21122053 <131326188+Sara21122053@users.noreply.github.com>
2023-06-08 17:35:51 -0500

    Update Reference.cs

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -15,31 +15,23 @@ public class Reference
         _FinalVerse = 0;
     }
 
-    public Reference (string reference)
+    public Reference (string book, int chapter, int initialVerse, int finalVerse)
     {
-        string[] parts = reference.Split(' ');
-        
-        if (parts.Length >= 3)
-        {
-            _Book = parts[0];
-            _Chapter = int.Parse(parts[1].Replace(":",""));
-            _InitialVerse = int.Parse(parts[2]);
-            if (parts.Length >= 5)
-            {
-                _FinalVerse = int.Parse(parts[4]);
-            }
-        }
+        _Book = book;
+        _Chapter = chapter;
+        _InitialVerse = initialVerse;
+        _FinalVerse = finalVerse;
     }
 
     public override string ToString()
     {
         if (_InitialVerse == _FinalVerse)
         {
-            return $"{_Book} {_Chapter}: {_InitialVerse}";
+            return $"{_Book } {_Chapter}: {_InitialVerse}";
         }
         else
         {
-            return $"{_Book} {_Chapter}: {_InitialVerse} - {_FinalVerse}";
+            return $"{_Book } {_Chapter}: {_InitialVerse} - {_FinalVerse}";
         }
     }
 }
