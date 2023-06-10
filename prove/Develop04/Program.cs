2586cd35bee1ae88496204b32c5e2a017f558ab1
Sara21122053 <131326188+Sara21122053@users.noreply.github.com>
2023-06-10 11:39:10 -0500

    Update Program.cs

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,70 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 class Program
 {
+    private static List<Activity> activities = new List<Activity>()
+        {
+            new BreathingActivity(),
+            new ReflectionActivity(),
+            new ListingActivity(),
+            new StretchingActivity()
+        };
+
+        private static List<string> usedPrompts = new List<string>();
+        private static string logFile = "Log.txt";
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        while (true)
+        {
+            ShowMenu();
+
+            int choice = GetMenuChoice();
+            if (choice == activities.Count + 1)
+            {
+                Console.WriteLine("¡Goodbye! Thank you for using the mindfulness program. Have a great day.");
+                break;
+            }
+
+            Activity activity = activities[choice - 1];
+            activity.Run();
+            LogActivity(activity);
+        }
+    }
+
+    static void ShowMenu()
+    {
+        Console.WriteLine("Welcome to Mindfulness Program");
+
+        for (int i = 0; i < activities.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {activities[i].Name}");
+        }
+
+        Console.WriteLine($"{activities.Count + 1}. Exit");
+    }
+
+    static int GetMenuChoice()
+    {
+        int choice;
+        while (true)
+        {
+            Console.WriteLine("Choose an activity");
+            if (int.TryParse(Console.ReadLine(), out choice) && choice >= 1 && choice <= activities.Count + 1)
+                break;
+
+            Console.WriteLine("Invalid option. Please, choose a valid option.");
+        }
+
+        Console.WriteLine();
+        return choice;
+    }
+
+    static void LogActivity(Activity activity)
+    {
+        string logEntry = $"{DateTime.Now}: {activity.Name}";
+        File.AppendAllText(logFile, logEntry + Environment.NewLine);
     }
 }
