61cc97050352c88e0fb4f8a6e2323b438dacb87d
Sara21122053 <131326188+Sara21122053@users.noreply.github.com>
2023-06-15 23:44:35 -0500

    Update Program.cs

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List <Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square ("Brown", 4);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle ("Green", 4, 3);
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle ("Pink", 4);
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine ($"The {color} shape has an area of {area}");
+        }
     }
 }
