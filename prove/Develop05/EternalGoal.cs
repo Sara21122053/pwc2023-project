0dad0c3dfaa712ba60ab0574e880a848a803633a
Sara21122053 <131326188+Sara21122053@users.noreply.github.com>
2023-07-15 23:17:48 -0500

    Update and rename Activity.cs to EternalGoal.cs

---
+++ prove/Develop05/EternalGoal.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class EternalGoal : Goal
+{
+    protected int _level;
+    protected int _pointsNecessaryByLevel;
+
+    public int Level
+    {
+        get {return _level;}
+        set {_level = value;}
+    }
+    public int PointsNecessaryByLevel
+    {
+        get {return _pointsNecessaryByLevel;}
+        set {_pointsNecessaryByLevel = value;}
+    }
+
+    public EternalGoal(string name, string description, int pointsForEachCompletion, int pointsNecessaryByLevel)
+    {
+        _name = name;
+        _description = description;
+        _pointsForEachCompletion = pointsForEachCompletion;
+        _pointsNecessaryByLevel = pointsNecessaryByLevel;
+        _level = 1;
+        _completed = false;
+        _pointsEarned = 0;
+    }
+
+    public override void CompleteGoal()
+    {
+        if (!_completed)
+        {
+            base.CompleteGoal();
+            if (_pointsEarned >= _pointsNecessaryByLevel)
+            {
+                _level++;
+                _pointsEarned = _pointsEarned - (_level - 1) * _pointsNecessaryByLevel;
+                _completed = true;
+            }
+        }
+    }
+
+    public override void SaveGoal(StreamWriter writer)
+    {
+       base.SaveGoal(writer);
+       writer.WriteLine("Level: " + _level);
+       writer.WriteLine("Points Necessary By Level: " + _pointsNecessaryByLevel);
+    }
+
+    public override void DisplayProgress()
+    {
+        Console.WriteLine((_completed ? "[ ]": "[ ]") + _name + " (" + _description + ") - Level: " + _level);
+    }
+}
