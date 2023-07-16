243b9aff1600ae820bdf900b944c08aa19905753
Sara21122053 <131326188+Sara21122053@users.noreply.github.com>
2023-07-16 00:11:21 -0500

    Update Program.cs

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Conference conference = new Conference("Conference Event", "Conference against discrimination in the labor and legal field", DateTime.Now, TimeSpan.FromHours(2), "456 Elm St", "Marco Harris", 100);
+        Reception reception = new Reception("Reception Event", "Reception to celebrate the baptism of a 15-year-old girl", DateTime.Now, TimeSpan.FromHours(3), "789 Oak St", "gilguero@gmail.com");
+        OutdoorMeeting outdoorMeeting = new OutdoorMeeting("Outdoor Meeting Event", "A 7-year-old's birthday will be celebrated outdoors along with a painting war", DateTime.Now, TimeSpan.FromHours(4), "321 Maple St", "Sunny");
+
+
+        Console.WriteLine("Conference Event - Standard Details:");
+        Console.WriteLine(conference.GetStandardDetails());
+
+        Console.WriteLine("\nConference Event - Full Details:");
+        Console.WriteLine(conference.GetFullDetails());
+
+        Console.WriteLine("\nConference Event - Short Description:");
+        Console.WriteLine(conference.GetShortDescription());
+
+
+        Console.WriteLine("\nReception Event - Standard Details:");
+        Console.WriteLine(reception.GetStandardDetails());
+
+        Console.WriteLine("\nReception Event - Full Details:");
+        Console.WriteLine(reception.GetFullDetails());
+
+        Console.WriteLine("\nReception Event - Short Description:");
+        Console.WriteLine(reception.GetShortDescription());
+
+
+        Console.WriteLine("\nOutdoor Meeting Event - Standard Details:");
+        Console.WriteLine(outdoorMeeting.GetStandardDetails());
+
+        Console.WriteLine("\nOutdoor Meeting Event - Full Details:");
+        Console.WriteLine(outdoorMeeting.GetFullDetails());
+
+        Console.WriteLine("\nOutdoor Meeting Event - Short Description:");
+        Console.WriteLine(outdoorMeeting.GetShortDescription());
     }
 }
