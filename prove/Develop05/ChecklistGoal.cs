67c56685b5801e5bb8d19c7a29adb74dc241179e
Sara21122053 <131326188+Sara21122053@users.noreply.github.com>
2023-07-15 23:18:34 -0500

    Update and rename GoTemple.cs to ChecklistGoal.cs

---
+++ prove/Develop05/ChecklistGoal.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class ChecklistGoal : Goal
+{
+    protected int _numberOfTimesToComplete;
+    protected int _numberOfTimesDone;
+    protected int _completionBonus;
+    protected int _numberNeddedForCompletion;
+
+    public int NumberOfTimesToComplete
+    {
+        get {return _numberOfTimesToComplete;}
+        set {_numberOfTimesToComplete = value;}
+    }
+
+    public int NumberOfTimesDone
+    {
+        get {return _numberOfTimesDone;}
+        set {_numberOfTimesDone = value;}
+    }
+
+    public int CompletionBonus
+    {
+        get {return _completionBonus;}
+        set {_completionBonus = value;}
+    }
+
+    public int NumberNeddedForCompletion
+    {
+        get {return _numberNeddedForCompletion;}
+        set {_numberNeddedForCompletion = value;}
+    }
+
+    public ChecklistGoal(string name, string description, int pointsForEachCompletion, int numberOfTimesToComplete, int numberOfTimesDone, int completionBonus, int numberNeddedForCompletion)
+    {
+        _name = name;
+        _description = description;
+        _pointsForEachCompletion = pointsForEachCompletion;
+        _numberOfTimesToComplete = numberOfTimesToComplete;
+        _numberOfTimesDone = numberOfTimesDone;
+        _completionBonus = completionBonus;
+        _numberNeddedForCompletion = numberNeddedForCompletion;
+        _completed = false;
+        _pointsEarned = 0;
+    }
+
+    public override void CompleteGoal()
+    {
+        if (!_completed)
+        {
+            base.CompleteGoal();
+            if (_numberOfTimesDone == _numberNeddedForCompletion)
+            {
+                _completed = true;
+                _pointsEarned = _numberOfTimesDone * _pointsForEachCompletion + _completionBonus;
+            }
+            else if (_numberOfTimesDone >= _numberOfTimesToComplete)
+            {
+                _completed = true;
+                _pointsEarned = _numberOfTimesToComplete * _pointsForEachCompletion;
+            }
+            else
+            {
+                _completed = false;
+                _pointsEarned = _numberOfTimesDone * _pointsForEachCompletion;
+            }
+        }
+    }
+
+    public override void SaveGoal(StreamWriter writer)
+    {
+        base.SaveGoal(writer);
+        writer.WriteLine("Number Of Times To Complete: " + _numberOfTimesToComplete);
+        writer.WriteLine("Number Of Times Done: " + _numberOfTimesDone);
+        writer.WriteLine("Completion Bonus: " + _completionBonus);
+        writer.WriteLine("Number Nedded For Completion: " + _numberNeddedForCompletion);
+    }
+
+    public override void DisplayProgress()
+    {
+        Console.WriteLine((_completed ? "[x]" : "[ ]") + _name + " (" + _description + ") - Currently completed: " + _numberOfTimesDone + " / " + _numberOfTimesToComplete);
+    }
+}
