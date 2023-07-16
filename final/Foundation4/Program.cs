f90e1cf40f20ed9fc55004753bf7b16d18451656
Sara21122053 <131326188+Sara21122053@users.noreply.github.com>
2023-07-16 00:27:43 -0500

    Update Program.cs

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Activity> activities = new List<Activity>();
+
+        DateTime _date = new DateTime(2022, 11, 3);
+        Activity running = new RunningActivity(_date, 30, 3.0);
+        Activity cycling = new CyclingActivity(_date, 30, 9.7);
+        Activity swimming = new SwimmingActivity(_date, 30, 10);
+
+        activities.Add(running);
+        activities.Add(cycling);
+        activities.Add(swimming);
+
+        foreach (Activity activity in activities)
+        {
+            string summary = activity.GetSummary();
+            Console.WriteLine(summary);
+        }
+    }
+}
     }
 }
