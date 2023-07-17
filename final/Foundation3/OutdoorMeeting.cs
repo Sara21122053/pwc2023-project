a1a5334d3c93e31c8df234ac4734e3bb1e49353d
Sara21122053 <131326188+Sara21122053@users.noreply.github.com>
2023-07-17 14:44:28 -0500

    Update OutdoorMeeting.cs

--- final/Foundation3/OutdoorMeeting.cs
+++ final/Foundation3/OutdoorMeeting.cs
@@ -4,14 +4,14 @@ class OutdoorMeeting : Event
 {
     private string _weatherForecast;
 
-    public OutdoorMeeting(string title, string description, DateTime date, TimeSpan time, string address, string weatherForecast)
-        : base(title, description, date, time, address)
+    public OutdoorMeeting(string type, string title, string description, DateTime date, TimeSpan time, string address, string weatherForecast)
+        : base(type, title, description, date, time, address)
     {
         this._weatherForecast = weatherForecast;
     }
 
     public override string GetFullDetails()
     {
-        return $"{base.GetFullDetails()}\nType: Outdoor Meeting\nWeather Forecast: {_weatherForecast}";
+        return $"{base.GetFullDetails()}\nType: {_type}\nWeather Forecast: {_weatherForecast}";
     }
 }
