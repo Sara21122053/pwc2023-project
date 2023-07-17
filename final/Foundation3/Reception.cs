082bea0f59f5f91d8956e5e1b0313a1b5b4291e1
Sara21122053 <131326188+Sara21122053@users.noreply.github.com>
2023-07-17 14:45:29 -0500

    Update Reception.cs

--- final/Foundation3/Reception.cs
+++ final/Foundation3/Reception.cs
@@ -4,13 +4,14 @@ class Reception: Event
 {
     private string _rsvpEmail;
 
-    public Reception(string title, string description, DateTime date, TimeSpan time, string address, string rsvpEmail) : base(title, description, date, time, address)
+    public Reception(string type, string title, string description, DateTime date, TimeSpan time, string address, string rsvpEmail) : base(type, title, description, date, time, address)
     {
         this._rsvpEmail = rsvpEmail;
     }
 
     public override string GetFullDetails()
     {
-        return $"{base.GetFullDetails()}\nType: Reception\nRSVP Email: {_rsvpEmail}";
+        return $"{base.GetFullDetails()}\nType: {_type}\nRSVP Email: {_rsvpEmail}";
     }
+    
 }
