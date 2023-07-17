2c2fea480807e1ef5e10a13b7258ca701a1a6b7b
Sara21122053 <131326188+Sara21122053@users.noreply.github.com>
2023-07-17 14:43:01 -0500

    Update Program.cs

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Conference conference = new Conference("Conference Event", "Conference against discrimination in the labor and legal field", DateTime.Now, TimeSpan.FromHours(2), "456 Elm St", "Marco Harris", 100);
-        Reception reception = new Reception("Reception Event", "Reception to celebrate the baptism of a 15-year-old girl", DateTime.Now, TimeSpan.FromHours(3), "789 Oak St", "gilguero@gmail.com");
-        OutdoorMeeting outdoorMeeting = new OutdoorMeeting("Outdoor Meeting Event", "A 7-year-old's birthday will be celebrated outdoors along with a painting war", DateTime.Now, TimeSpan.FromHours(4), "321 Maple St", "Sunny");
+        Conference conference = new Conference("Academic", "Good Living Forum", "Conference against discrimination in the labor and legal field", DateTime.Now, TimeSpan.FromHours(2), "456 Elm St", "Marco Harris", 100);
+        Reception reception = new Reception("Social", "Nina's Baptism", "Reception to celebrate the baptism of a 15-year-old girl", DateTime.Now, TimeSpan.FromHours(3), "789 Oak St", "gilguero@gmail.com");
+        OutdoorMeeting outdoorMeeting = new OutdoorMeeting("Social", "Jerom's Birthday", "A 7-year-old's birthday will be celebrated outdoors along with a painting war", DateTime.Now, TimeSpan.FromHours(4), "321 Maple St", "Sunny");
 
 
         Console.WriteLine("Conference Event - Standard Details:");
