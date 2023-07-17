2bfa3ee87a0af7d2a1f650408805cc99eda11855
Sara21122053 <131326188+Sara21122053@users.noreply.github.com>
2023-07-17 14:43:54 -0500

    Update Conference.cs

--- final/Foundation3/Conference.cs
+++ final/Foundation3/Conference.cs
@@ -5,7 +5,7 @@ class Conference : Event
     private string _speakerName;
     private int _capacity;
 
-    public Conference(string title, string description, DateTime date, TimeSpan time, string address, string speakerName, int capacity) : base(title, description, date, time, address)
+    public Conference(string type, string title, string description, DateTime date, TimeSpan time, string address, string speakerName, int capacity) : base(type, title, description, date, time, address)
     {
         this._speakerName = speakerName;
         this._capacity = capacity;
@@ -13,6 +13,6 @@ class Conference : Event
 
     public override string GetFullDetails()
     {
-        return $"{base.GetFullDetails()}\nType: Conference\nSpeaker: {_speakerName}\nCapacity: {_capacity}";
+        return $"{base.GetFullDetails()}\nType: {_type}\nSpeaker: {_speakerName}\nCapacity: {_capacity}";
     }
 }
